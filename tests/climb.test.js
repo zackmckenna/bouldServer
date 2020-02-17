7e1d6e78a2aa948a540b5eaff67e549c1a2e1e5f
Zack McKenna <zackmckenna@gmail.com>
2020-02-16 21:22:55 -0500

    added node envs to package.json

--- tests/climb.test.js
+++ tests/climb.test.js
@@ -6,3 +6,32 @@ test('dummy returns one', () => {
   const result = climbHelper.dummy(climbs)
   expect(result).toBe(1)
 })
+
+test('personal difficulty is a number', () => {
+  const climb = {
+    id: 3,
+    personalDifficulty: 3,
+    setDifficulty: 4,
+    result: 'uncomplete',
+    completed: false
+  }
+
+  const result = climbHelper.personalDifficultyIsNumber(climb)
+  expect(result).toBe(3)
+})
+
+// const personalDifficultyIsNumber = climb => {
+//   return climb.personalDifficulty
+// }
+
+// const setDifficultyIsNumber = climb => {
+//   return climb.setDifficulty
+// }
+
+// const resultIsString = climb => {
+//   return climb.result
+// }
+
+// const completedIsBoolean = climb => {
+//   return climb.completed
+// }
--- utils/climb_helper.js
+++ utils/climb_helper.js
@@ -1,7 +1,28 @@
-const dummy = climbs => {
+const dummy = () => {
   return 1
 }
 
+const personalDifficultyIsNumber = climb => {
+  return climb.personalDifficulty
+}
+
+const setDifficultyIsNumber = climb => {
+  return climb.setDifficulty
+}
+
+const resultIsString = climb => {
+  return climb.result
+}
+
+const completedIsBoolean = climb => {
+  return climb.completed
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  personalDifficultyIsNumber,
+  setDifficultyIsNumber,
+  resultIsString,
+  completedIsBoolean
 }
