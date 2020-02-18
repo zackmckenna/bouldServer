c109095ccd9e23eca92bf0b03963fb2070c4e0ec
Zack McKenna <zackmckenna@gmail.com>
2020-02-18 12:47:59 -0500

    revert mongoDB changes

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ const mongoose = require('mongoose')
 
 logger.info('connecting to', config.MONGODB_URI)
 
-console.log(config)
-console.log(config.MONGODB_URI)
-
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     logger.info('connected to MongoDB')
